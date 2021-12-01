96e9557b3307a9c2fa307bac8d3046da05d76d97
mehdidevv <mehdi.saber.700@gmail.com>
2021-12-02 00:19:54 +0100

    "second commit"

---
+++ TD6/app/src/main/java/com/mashy/td6/MainActivity.java
@@ -0,0 +1,55 @@
+package com.mashy.td6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+public class MainActivity extends AppCompatActivity {
+    List<Repo> reposList=new ArrayList<Repo>();
+    Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repo);
+        RecyclerView rvContacts=(RecyclerView) findViewById(R.id.rvContacts);
+        context=this;
+        GithubService githubService=new Retrofit.Builder().baseUrl(GithubService.ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GithubService.class);
+
+        githubService.listRepos("adrienbusin").enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+                reposList=response.body();
+                afficherRepos(response.body());
+                ReposAdapter adapter=new ReposAdapter(reposList);
+                rvContacts.setAdapter(adapter);
+                rvContacts.setLayoutManager(new LinearLayoutManager(context));
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Repo>> call, Throwable t) {
+                t.getCause();
+            }
+        });
+    }
+    public void afficherRepos(List<Repo> repos) {
+        Toast.makeText(this,"nombre de dépots : "+repos.size(), Toast.LENGTH_SHORT).show();
+    }
+}
+
+
